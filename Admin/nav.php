172f6f359b68479655337014a6594702f67e781b
aqnodj <aquinodm@students.nu-lipa.edu.ph>
2024-10-27 10:07:46 +0000

    Add files via upload

---
+++ Admin/nav.php
@@ -0,0 +1,18 @@
+
+<center>
+<a href="index" class="btn-primary">Main</a>
+
+&nbsp;
+&nbsp;
+<a href="ViewRecord" class="btn-primary"> View Records</a>
+
+&nbsp;
+&nbsp;
+
+<a href="SendSMS" class="btn-primary"> Send SMS</a>
+
+&nbsp;
+&nbsp;
+
+<a href="Setting" class="btn-primary">Setting</a>
+</center>
